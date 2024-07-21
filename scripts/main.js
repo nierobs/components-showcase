d3cd45f7b1c923e18f575fcca2fb351a9cb003d7
Robson Niemeyer <nierobs@hotmail.com>
2024-07-21 01:21:25 -0300

    Update script

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,20 @@
  */
 const nodes = document.getElementsByClassName('animation--fade-in');
 
-for (let i = 0; i < nodes.length; i++) {
-    if (window.innerHeight - nodes[i].offsetTop <= 0) {
-        nodes[i].style.opacity = 0;
-    }
-}
-
-window.addEventListener('scroll', (event) => {
+window.addEventListener('load', () => {
     for (let i = 0; i < nodes.length; i++) {
-        if (window.innerHeight + window.scrollY - nodes[i].offsetTop > 0) {
-            nodes[i].classList.add('animation--fade-in--show');
+        if (window.scrollY + window.innerHeight - nodes[i].offsetTop <= 0) {
+            nodes[i].style.opacity = 0;
         }
     }
+
+    window.addEventListener('scroll', () => {
+        for (let i = 0; i < nodes.length; i++) {
+            if (window.scrollY + window.innerHeight - nodes[i].offsetTop > 0) {
+                nodes[i].classList.add('animation--fade-in--show');
+            }
+        }
+    });
 });
 
 /* biati-digital/glightbox
